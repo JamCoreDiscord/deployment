9fa7d8d97140b8ef5c6998243d4ca5ef4796bf90
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-24 22:48:27 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var childProcess = require("child_process");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get("/ping", (req, res, next) => {
+    res.json({ success: true });
+});
+
+app.post("/notify", (req, res, next) => {
+    //Input will be: {"build_url": https://github.com/JamCoreDiscord/builds/tree/main/...}
+    var url = req.body.build_url
+    console.log("Recieived update with URL: " + url)
+
+    res.sendStatus(200)
+
+    childProcess.exec('sh updatePinguino.sh ' + url,
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`Error Updating Pinguino: ${error}`);
+            }
+        });
+});
+
+app.listen(3000, () => {
+    console.log("Deployment API running on port 3000!");
+});
