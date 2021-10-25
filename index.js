f233eddee99409701c678e4aedfaeee22edafbe6
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-25 12:02:50 +0100

    Check update is actually for pingunio

--- index.js
+++ index.js
@@ -11,20 +11,22 @@ app.get("/ping", (req, res, next) => {
 
 app.post("/notify", (req, res, next) => {
     //Input will be: {"build_url": https://github.com/JamCoreDiscord/builds/tree/main/...}
-    var url = req.body.build_url
-    url = url.replace("tree", "raw")
-    console.log("Recieived update with URL: " + url)
+    var url = req.body.build_url;
+    url = url.replace("tree", "raw");
+    console.log("Recieived update with URL: " + url);
 
-    res.sendStatus(200)
+    res.sendStatus(200);
 
-    childProcess.exec('sh updatePinguino.sh ' + url,
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`Error Updating Pinguino: ${error}`);
-            }
-        });
+    if (url.includes("Pinguino") && url.includes(".jar")) {
+        childProcess.exec('sh updatePinguino.sh ' + url,
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+                if (error !== null) {
+                    console.log(`Error Updating Pinguino: ${error}`);
+                }
+            });
+    }
 });
 
 app.listen(3000, () => {
