e5f05559a24fb70e95feb1c5dc0191aba7ae47fe
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-12-23 23:22:08 +0000

    E

--- index.js
+++ index.js
@@ -6,29 +6,46 @@ var app = express();
 app.use(bodyParser.json());
 
 app.get("/ping", (req, res, next) => {
-    res.json({ success: true });
+  res.json({ success: true });
 });
 
 app.post("/notify", (req, res, next) => {
-    //Input will be: {"build_url": https://github.com/JamCoreDiscord/builds/tree/main/...}
-    var url = req.body.build_url;
-    url = url.replace("tree", "raw");
-    console.log("Recieived update with URL: " + url);
+  //Input will be: {"build_url": https://github.com/JamCoreDiscord/builds/tree/main/...}
+  var url = req.body.build_url;
+  url = url.replace("tree", "raw");
+  console.log("Recieived update with URL: " + url);
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 
-    if (url.includes("Pinguino") && url.includes(".jar")) {
-        childProcess.exec('sh updatePinguino.sh ' + url,
-            (error, stdout, stderr) => {
-                console.log(stdout);
-                console.log(stderr);
-                if (error !== null) {
-                    console.log(`Error Updating Pinguino: ${error}`);
-                }
-            });
+  if (url.includes("Pinguino") && url.includes(".jar")) {
+    childProcess.exec(
+      "sh updatePinguino.sh " + url,
+      (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+          console.log(`Error Updating Pinguino: ${error}`);
+        }
+      }
+    );
+  }
+});
+
+app.post("/deploy/dog", (req, res, next) => {
+  childProcess;
+  childProcess.exec(
+    "./deploy.sh",
+    { cwd: "~/DogWebsite" },
+    (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (error !== null) {
+        console.log(`Error Updating dog.jamalam.tech: ${error}`);
+      }
     }
+  );
 });
 
 app.listen(3000, () => {
-    console.log("Deployment API running on port 3000!");
+  console.log("Deployment API running on port 3000!");
 });
